81cc7ae9386717b8e004584a4c32a1699a73c216
Luigi.Robles <Luigi.robles0846@my.riohondo.edu>
2020-08-01 22:19:16 -0700

    Add project files.

---
+++ CIT125_Ch12_Fig_12-14/CIT125_Ch12_Fig_12-14.cpp
@@ -0,0 +1,48 @@
+// CIT125 Intro to C++ Luigi Robles
+// 08-01-2020 Summer term
+// Ch.12 pg.443 Fig.12-14 
+// modified region sales program
+// Gets and displays order amounts
+
+#include <iostream>
+using namespace std;
+
+//function prototype
+void displayArray(int nums[4][3]);
+
+int main()
+{
+	//declare and initialize array
+	int orders[4][3] = {0};
+
+	//store data in the array
+	for (int region = 0; region < 4; region += 1)
+		for (int month = 0; month < 3; month += 1)
+		{
+			cout << "Region " << region + 1
+				<< ", Month " << month + 1
+				<< " orders: ";
+			cin >> orders[region][month]; //input for regions and months into array
+		}	//end for
+	//end for
+
+	//display the contents of the array
+	displayArray(orders);
+	return 0;
+} //end of main function
+
+//*****function definitions*****
+void displayArray(int nums[4][3])
+{
+	cout << endl << "Array contents: " << endl;
+	for (int region = 0; region < 4; region += 1)
+	{
+		cout << "Region " << region + 1
+			<< ": " << endl;
+		for (int month = 0; month < 3; month += 1)
+			cout << "	Month " << month + 1
+			<< ": " << nums[region][month]
+			<< endl;
+		//end for
+	} //end for
+}	//end of displayArray function
